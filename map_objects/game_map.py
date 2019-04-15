4faf15eeddde724476e19663a4c7bb94b4964aee
gdl68 <26163963+gdl68@users.noreply.github.com>
2019-04-16 00:00:29 +0300

    Part 6: Hell yeah!
    - Fighting system
    - Walking orcs
    - Attacking trolls
    - Running away
    - I fucking burned out

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -3,7 +3,9 @@ from map_objects.rectangle import Rect
 from random import randint
 from entity import Entity
 import tcod as libtcod
-
+from components.ai import BasicMonster
+from components.fighter import Fighter
+from render_function import RenderOrder
 
 class GameMap:
     def __init__(self, width, height):
@@ -102,9 +104,19 @@ class GameMap:
 
             if not any([entity for entity in entities if (entity.x == x and entity.y == y)]):
                 if randint(0, 100) < 80:
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
+
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
+                                     render_order=RenderOrder.ACTOR,
+                                     fighter=fighter_component, ai=ai_component)
                 else:
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+
+                    monster = Entity(x, y, 'T', libtcod.desaturated_green, 'Troll', blocks=True,
+                                     render_order=RenderOrder.ACTOR,
+                                     fighter=fighter_component, ai=ai_component)
 
                 entities.append(monster)
 
