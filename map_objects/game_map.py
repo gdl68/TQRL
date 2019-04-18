03c09ac1370f6ddcb71854980d26680cedd9c35d
gdl68 <26163963+gdl68@users.noreply.github.com>
2019-04-18 21:41:16 +0300

    Part 8: Inventory/Item system
    - Added inventory system
    - Added first item, Heal potion lv. 1
    - You can grab, use, and drop items!

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -6,6 +6,9 @@ import tcod as libtcod
 from components.ai import BasicMonster
 from components.fighter import Fighter
 from render_function import RenderOrder
+from components.item import Item
+from item_functions import heal
+
 
 class GameMap:
     def __init__(self, width, height):
@@ -18,7 +21,7 @@ class GameMap:
         return tiles
 
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
-                 max_monsters_per_room):
+                 max_monsters_per_room, max_items_per_room):
         # Create two rooms fore demonstration purposes
         rooms = []
         num_rooms = 0
@@ -70,7 +73,7 @@ class GameMap:
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
                 # finally, append the new room to the list
-                self.place_entities(new_room, entities, max_monsters_per_room)
+                self.place_entities(new_room, entities, max_monsters_per_room, max_items_per_room)
 
                 rooms.append(new_room)
                 num_rooms += 1
@@ -93,10 +96,12 @@ class GameMap:
             self.tiles[x][y].block_sight = False
 
     # UNDERSTAND THIS LATER
-    def place_entities(self, room, entities, max_monsters_per_room):
+    def place_entities(self, room, entities, max_monsters_per_room, max_items_per_room):
         # Get a random number of monsters
         number_of_monsters = randint(0, max_monsters_per_room)
+        number_of_items = randint(0, max_items_per_room)
 
+        # Monster cycle
         for i in range(number_of_monsters):
             # Choose a random location in the room
             x = randint(room.x1 + 1, room.x2 - 1)
@@ -119,6 +124,19 @@ class GameMap:
                                      fighter=fighter_component, ai=ai_component)
 
                 entities.append(monster)
+
+        # Item cycle
+        for i in range(max_items_per_room):
+            # Choose a random location
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if (entity.x == x and entity.y == y)]):
+                item_component = Item(use_function=heal, amount=4)
+                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion lv.1', render_order=RenderOrder.ITEM,
+                              item=item_component)
+
+                entities.append(item)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
